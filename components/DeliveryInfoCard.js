b4c6a2cf87ad3832764ea905d777360ac04b29e3
JamesL1408 <jvjllewellyn@gmail.com>
2022-05-25 20:26:40 +0200

    fifth commit

--- components/DeliveryInfoCard.js
+++ components/DeliveryInfoCard.js
@@ -4,24 +4,25 @@ import DeliveryImage from '../images/Steak.jpeg'
 
 function DeliveryInfoCard() {
   return (
-    <div className='w-full h-min rounded-md flex text-center items-center shadow-md shadow-gray-400 select-none relative'>
+    <div >
 
         {/* Writing Layer */}
-        <div className='flex flex-col items-center justify-center px-5 py-5 bg-transparent rounded-md space-y-7 z-20 text-white'>
+        <a href="" className='w-full h-min rounded-md flex text-center items-center shadow-md shadow-gray-400 select-none relative group'>
+        <div className='flex flex-col items-center justify-center px-5 py-5 bg-transparent rounded-md space-y-7 z-20 text-white group-hover:opacity-40 transition-all duration-500 ease-in-out'>
             <h1 className=' text-xl font-semibold'>Deliveries</h1>
             <div className='h-[2px] w-[80%] bg-white'></div>
             <p>Be the first to hear about special offers and promotions, by following us on social media or signing up for our newsletter. </p>
-            <a className='py-2 px-4 bg-transparent rounded-md text-xs font-semibold text-white font-Roboto shadow-sm shadow-gray-500 transition-all duration-300 hover:bg-Jet hover:text-white ease-in-out border border-white' href="">TAKE A LOOK  {'>'}</a>
+            <a className='py-2 px-4 bg-transparent rounded-md text-xs font-semibold text-white font-Roboto shadow-sm shadow-gray-500  border border-white' href="">TAKE A LOOK  {'>'}</a>
         </div>
 
         {/* Opacity Layer */}
-        <div className='absolute w-full h-full rounded-md bg-black bg-opacity-60 top-0 left-0 z-10 '></div>
+        <div className='absolute w-full h-full rounded-md bg-black bg-opacity-70 group-hover:bg-opacity-0 transition-all duration-500 ease-in-out top-0 left-0 z-10 '></div>
 
 
         {/* Image Layer */}
-        <div className='absolute w-full h-full rounded-md  z-0'><Image className='rounded-md z-0' layout='fill' src={DeliveryImage}/></div>
+        <div className='absolute w-full h-full rounded-md z-0 overflow-hidden'><Image className='rounded-md z-0 group-hover:scale-110 transition-all duration-500 ease-in-out' layout='fill' src={DeliveryImage}/></div>
 
-        
+        </a>
         
     </div>
   )
--- components/EventsInfoCard.js
+++ components/EventsInfoCard.js
@@ -4,26 +4,24 @@ import EventsImg from '../images/EventImage.jpg'
 
 function EventsInfoCard() {
   return (
-    <div className='w-full h-min rounded-md flex text-center items-center shadow-md shadow-gray-400 select-none relative'>
+    <div >
 
         {/* Writing Layer */}
-       
-        <div className='flex flex-col items-center justify-center px-5 py-5 bg-transparent rounded-md space-y-7 z-20 text-white'>
+       <a href="" className='w-full h-min rounded-md flex text-center items-center shadow-md shadow-gray-400 select-none relative group'>
+        <div className='flex flex-col items-center justify-center px-5 py-5 bg-transparent rounded-md space-y-7 z-20 text-white group-hover:opacity-40 transition-all duration-500 ease-in-out '>
             <h1 className=' text-xl font-semibold'>Events {'&'} Offers</h1>
             <div className='h-[2px] w-[80%] bg-white'></div>
             <p>Be the first to hear about special offers and promotions, by following us on social media or signing up for our newsletter. </p>
-            <a className='py-2 px-4 bg-Classy rounded-md text-xs font-semibold text-Jet font-Roboto shadow-sm shadow-gray-500 transition-all duration-300 hover:bg-Jet hover:text-white ease-in-out border border-white' href="">TAKE A LOOK  {'>'}</a>
+            <a className='py-2 px-4 bg-Classy rounded-md text-xs font-semibold text-Jet font-Roboto shadow-sm shadow-gray-500  border border-white' href="">TAKE A LOOK  {'>'}</a>
         </div>
 
         {/* Opacity Layer */}
-        <div className='absolute w-full h-full rounded-md bg-black bg-opacity-60 top-0 left-0 z-10 '></div>
+        <div className='absolute w-full h-full rounded-md bg-black bg-opacity-70 top-0 left-0 z-10 group-hover:bg-opacity-0 transition-all duration-500 ease-in-out '></div>
 
 
         {/* Image Layer */}
-        <div className='absolute w-full h-full rounded-md  z-0'><Image className='rounded-md z-0' layout='fill' src={EventsImg}/></div>
-
-        
-        
+        <div className='absolute w-full h-full rounded-md  overflow-hidden z-0'><Image className='rounded-md z-0 group-hover:scale-110 transition-all duration-500 ease-in-out' layout='fill' src={EventsImg}/></div>
+</a>
     </div>
   )
 }
